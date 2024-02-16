d56c3a5ae955a7ac0f72135b36c788bce6a234fd
Kunal Dubey <mailme@kunaldubey.com>
2024-02-16 22:57:06 +0530

    added comments

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,18 +6,18 @@ the game should tell you higher or lower until you reach the number
 PS: Basically, Binary Search on 1-100
 */ 
 
-use std::io;
+use std::io; // This is because the IO library isn't generally included in the standard library 
 
 fn main() {
     println!("*** Guess the number ***"); 
 
     println!("Enter your guess");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // mut because the guess string is mutable 
 
     io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read the input"); 
+        .read_line(&mut guess) // store it in the guess variable
+        .expect("Failed to read the input");  // like a catch block
     
-    println!("You guessed: {guess}");
+    println!("You guessed: {guess}"); 
 }
