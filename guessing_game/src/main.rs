d75e8e9210d4c8b6604c3d45cfed10cc33e482e9
Kunal Dubey <mailme@kunaldubey.com>
2024-02-27 00:49:01 +0530

    Completed Guessing Game Chapter

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,20 +4,38 @@ Such that if you input a number
 the game should tell you higher or lower until you reach the number 
 
 PS: Basically, Binary Search on 1-100
-*/ 
+*/
 
-use std::io; // This is because the IO library isn't generally included in the standard library 
+use std::io; // This is because the IO library isn't generally included in the standard library
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("*** Guess the number ***"); 
+    println!("*** Guess the number ***");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let mut tries = 0;
+    loop {
+        println!("Enter your guess");
+        let mut guess = String::new(); // mut because the guess string is mutable
 
-    println!("Enter your guess");
+        io::stdin()
+            .read_line(&mut guess) // store it in the guess variable
+            .expect("Failed to read the input");  // like a catch block
 
-    let mut guess = String::new(); // mut because the guess string is mutable 
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_)=>continue,
+        };
 
-    io::stdin()
-        .read_line(&mut guess) // store it in the guess variable
-        .expect("Failed to read the input");  // like a catch block
-    
-    println!("You guessed: {guess}"); 
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too Small"),
+            Ordering::Equal => {
+                println!("You guessed it right");
+                break;
+            },
+            Ordering::Greater=> println!("Too Big")
+        }
+        tries+=1;
+    }
+    println!("You took {tries} Tries");
 }
